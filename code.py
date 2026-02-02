111ab528baca86bee8b70e8c892f23553f7bee2b
Sanjana H S <sanjugangatkar068@gmail.com>
2026-02-02 12:34:25 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,7 @@
 import random
  import string 
 def generate_password(length, use_upper, use_lower, use_digits, use_symbols): characters = ""
+
+if use_upper:
+        characters +=
+ string.ascii_uppercase
